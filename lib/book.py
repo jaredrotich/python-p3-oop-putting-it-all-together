4da236ae161f92b58cd73665d91be1264ee8848d
jared <jaredrotich3@gmail.com>
2025-05-30 16:30:30 +0300

    worked on object oriented programme

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+   class Book:
+    def __init__(self, title="", page_count=0, genre=""):
+        self.title = title
+        self.page_count = page_count
+        self.genre = genre
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    # Validation using property for page_count
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, value):
+        if isinstance(value, int) and value > 0:
+            self._page_count = value
+        else:
+            print("page_count must be a positive integer.")
+
+    page_count = property(get_page_count, set_page_count)
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+   class Shoe:
+    def __init__(self, brand="", size=0):
+        self.brand = brand
+        self.size = size
+        self.condition = ""
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    # Validation using property for size
+    def get_size(self):
+        return self._size
+
+    def set_size(self, value):
+        if isinstance(value, (int, float)) and value > 0:
+            self._size = value
+        else:
+            print("size must be a positive number.")
+
+    size = property(get_size, set_size)
